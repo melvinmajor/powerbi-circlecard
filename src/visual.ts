3208d5941c6c79d60e10904399768e1c6e5f7605
Melvin Campos Casares <melvin.campos@outlook.be>
2021-06-02 17:18:12 +0200

    Cleaned visual.ts

--- src/visual.ts
+++ src/visual.ts
@@ -40,12 +40,7 @@ import IVisualHost = powerbi.extensibility.IVisualHost;
 import * as d3 from "d3";
 type Selection<T extends d3.BaseType> = d3.Selection<T, any,any, any>;
 
-/* import { VisualSettings } from "./settings"; */
 export class Visual implements IVisual {
-/*    private target: HTMLElement;
- *    private updateCount: number;
- *    private settings: VisualSettings;
- *    private textNode: Text; */
 
     private host: IVisualHost;
     private svg: Selection<SVGElement>;
@@ -55,19 +50,6 @@ export class Visual implements IVisual {
     private textLabel: Selection<SVGElement>;
 
     constructor(options: VisualConstructorOptions) {
-/*         console.log('Visual constructor', options);
- *        this.target = options.element;
- *        this.updateCount = 0;
- *        if (document) {
- *            const new_p: HTMLElement = document.createElement("p");
- *            new_p.appendChild(document.createTextNode("Update count:"));
- *            const new_em: HTMLElement = document.createElement("em");
- *            this.textNode = document.createTextNode(this.updateCount.toString());
- *            new_em.appendChild(this.textNode);
- *            new_p.appendChild(new_em);
- *            this.target.appendChild(new_p);
-        } */
-
         this.svg = d3.select(options.element)
             .append('svg')
             .classed('circleCard', true);
@@ -82,12 +64,6 @@ export class Visual implements IVisual {
     }
 
     public update(options: VisualUpdateOptions) {
-/*        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
- *        console.log('Visual update', options);
- *        if (this.textNode) {
- *            this.textNode.textContent = (this.updateCount++).toString();
-        } */
-
         let dataView: DataView = options.dataViews[0];
 
         let width: number = options.viewport.width;
@@ -120,17 +96,4 @@ export class Visual implements IVisual {
             .attr("text-anchor", "middle")
             .style("font-size", fontSizeLabel + "px");
     }
-
-/*    private static parseSettings(dataView: DataView): VisualSettings {
- *        return <VisualSettings>VisualSettings.parse(dataView);
-    } */
-
-    /**
-     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
-     * objects and properties you want to expose to the users in the property pane.
-     *
-     */
-/*    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
- *        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
-    } */
 }
